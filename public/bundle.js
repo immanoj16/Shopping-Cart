da4ebd23e8fa1168a4e55ad25c56687c3f6026e0
immanoj16 <mmaharana14@gmail.com>
2017-09-18 23:53:09 +0530

    Add: combine reducer

--- public/bundle.js
+++ public/bundle.js
@@ -70,60 +70,19 @@
 "use strict";
 
 
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
-
 var _redux = __webpack_require__(8);
 
-function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+var _reducers = __webpack_require__(24);
 
-// STEP 3 define reducers
-var reducer = function reducer() {
-  var state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : { books: [] };
-  var action = arguments[1];
+var _reducers2 = _interopRequireDefault(_reducers);
 
-  switch (action.type) {
-    case "POST_BOOK":
-      // let books = state.books.concat(action.payload);
-      // return {books};
-      return { books: [].concat(_toConsumableArray(state.books), _toConsumableArray(action.payload)) };
-
-    case "DELETE_BOOK":
-      // Create a copy of the current array of books
-      var currentBookToDelete = [].concat(_toConsumableArray(state.books));
-
-      // Determine at which index in books array is the book to be deleted
-      var indexToDelete = currentBookToDelete.findIndex(function (book) {
-        return book.id === action.payload.id;
-      });
-
-      // use slice to remove the book at the specified index
-      return { books: [].concat(_toConsumableArray(currentBookToDelete.slice(0, indexToDelete)), _toConsumableArray(currentBookToDelete.slice(indexToDelete + 1))) };
-
-    case "UPDATE_BOOK":
-      // Create a copy of the current array of books
-      var currentBookToUpdate = [].concat(_toConsumableArray(state.books));
-
-      // Determine at which index in books array is the book to be updated
-      var indexToUpdate = currentBookToUpdate.findIndex(function (book) {
-        return book.id === action.payload.id;
-      });
-
-      // Create a new book object with the new values and with the same array index of the item we want to replace.
-      // To achieve this we will use ...spread but we could use concat methods too
-      var newBookToUpdate = _extends({}, currentBookToUpdate[indexToUpdate], {
-        title: action.payload.title
-      });
-
-      // This Log has the purpose to show you how newBookToUpdate looks like
-      console.log("what is it newBookToUpdate", newBookToUpdate);
-      // use slice to remove the book at the specified index, replace with the new object and concatenate with the rest of the items in the array
-      return { books: [].concat(_toConsumableArray(currentBookToUpdate.slice(0, indexToUpdate)), [newBookToUpdate], _toConsumableArray(currentBookToUpdate.slice(indexToUpdate + 1))) };
-  }
-  return state;
-};
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 // STEP 1 create the store
-var store = (0, _redux.createStore)(reducer);
+var store = (0, _redux.createStore)(_reducers2.default);
+
+// IMPORT COMBINED REDUCERS
+
 
 store.subscribe(function () {
   console.log('current state is: ', store.getState());
@@ -1397,6 +1356,92 @@ function applyMiddleware() {
       });
     };
   };
+}
+
+/***/ }),
+/* 24 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _redux = __webpack_require__(8);
+
+var _booksReducers = __webpack_require__(25);
+
+// HERE COMBINE THE REDUCERS
+exports.default = (0, _redux.combineReducers)({
+  books: _booksReducers.booksReducers
+});
+
+// HERE IMPORT REDUCERS TO BE COMBINED
+
+/***/ }),
+/* 25 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+// BOOKS REDUCERS
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+exports.booksReducers = booksReducers;
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+function booksReducers() {
+  var state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : { books: [] };
+  var action = arguments[1];
+
+  switch (action.type) {
+    case "POST_BOOK":
+      // let books = state.books.concat(action.payload);
+      // return {books};
+      return { books: [].concat(_toConsumableArray(state.books), _toConsumableArray(action.payload)) };
+
+    case "DELETE_BOOK":
+      // Create a copy of the current array of books
+      var currentBookToDelete = [].concat(_toConsumableArray(state.books));
+
+      // Determine at which index in books array is the book to be deleted
+      var indexToDelete = currentBookToDelete.findIndex(function (book) {
+        return book.id === action.payload.id;
+      });
+
+      // use slice to remove the book at the specified index
+      return { books: [].concat(_toConsumableArray(currentBookToDelete.slice(0, indexToDelete)), _toConsumableArray(currentBookToDelete.slice(indexToDelete + 1))) };
+
+    case "UPDATE_BOOK":
+      // Create a copy of the current array of books
+      var currentBookToUpdate = [].concat(_toConsumableArray(state.books));
+
+      // Determine at which index in books array is the book to be updated
+      var indexToUpdate = currentBookToUpdate.findIndex(function (book) {
+        return book.id === action.payload.id;
+      });
+
+      // Create a new book object with the new values and with the same array index of the item we want to replace.
+      // To achieve this we will use ...spread but we could use concat methods too
+      var newBookToUpdate = _extends({}, currentBookToUpdate[indexToUpdate], {
+        title: action.payload.title
+      });
+
+      // This Log has the purpose to show you how newBookToUpdate looks like
+      console.log("what is it newBookToUpdate", newBookToUpdate);
+      // use slice to remove the book at the specified index, replace with the new object and concatenate with the rest of the items in the array
+      return { books: [].concat(_toConsumableArray(currentBookToUpdate.slice(0, indexToUpdate)), [newBookToUpdate], _toConsumableArray(currentBookToUpdate.slice(indexToUpdate + 1))) };
+  }
+  return state;
 }
 
 /***/ })
--- src/app.js
+++ src/app.js
@@ -1,52 +1,11 @@
 "use strict"
 import { createStore } from 'redux';
 
-// STEP 3 define reducers
-const reducer = function (state={books: []}, action) {
-  switch (action.type) {
-    case "POST_BOOK":
-      // let books = state.books.concat(action.payload);
-      // return {books};
-      return {books: [...state.books, ...action.payload]};
-
-    case "DELETE_BOOK":
-      // Create a copy of the current array of books
-      const currentBookToDelete = [...state.books];
-
-      // Determine at which index in books array is the book to be deleted
-      const indexToDelete = currentBookToDelete.findIndex(function (book) {
-        return book.id === action.payload.id
-      });
-
-      // use slice to remove the book at the specified index
-      return {books: [...currentBookToDelete.slice(0, indexToDelete), ...currentBookToDelete.slice(indexToDelete + 1)]}
-
-    case "UPDATE_BOOK":
-      // Create a copy of the current array of books
-      const currentBookToUpdate = [...state.books];
-
-      // Determine at which index in books array is the book to be updated
-      const indexToUpdate = currentBookToUpdate.findIndex(function (book) {
-        return book.id === action.payload.id
-      });
-
-      // Create a new book object with the new values and with the same array index of the item we want to replace.
-      // To achieve this we will use ...spread but we could use concat methods too
-      const newBookToUpdate = {
-        ...currentBookToUpdate[indexToUpdate],
-        title: action.payload.title
-      };
-
-      // This Log has the purpose to show you how newBookToUpdate looks like
-      console.log("what is it newBookToUpdate", newBookToUpdate);
-      // use slice to remove the book at the specified index, replace with the new object and concatenate with the rest of the items in the array
-      return {books: [...currentBookToUpdate.slice(0, indexToUpdate), newBookToUpdate, ...currentBookToUpdate.slice(indexToUpdate + 1)]}
-  }
-  return state;
-};
+// IMPORT COMBINED REDUCERS
+import reducers from './reducers';
 
 // STEP 1 create the store
-const store = createStore(reducer);
+const store = createStore(reducers);
 
 store.subscribe(function () {
   console.log('current state is: ', store.getState());
