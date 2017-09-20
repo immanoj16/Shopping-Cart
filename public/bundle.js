87334c97174701cdc5cfb8de2c81be85894f8cef
immanoj16 <mmaharana14@gmail.com>
2017-09-20 20:58:01 +0530

    Add: delete book feature

--- public/bundle.js
+++ public/bundle.js
@@ -32717,7 +32717,7 @@ function booksReducers() {
 
       // Determine at which index in books array is the book to be deleted
       var indexToDelete = currentBookToDelete.findIndex(function (book) {
-        return book._id === action.payload._id;
+        return book._id == action.payload;
       });
 
       // use slice to remove the book at the specified index
@@ -44162,8 +44162,24 @@ var BooksForm = function (_Component) {
       this.props.postBooks(book);
     }
   }, {
+    key: 'onDelete',
+    value: function onDelete() {
+      var bookId = (0, _reactDom.findDOMNode)(this.refs.delete).value;
+
+      this.props.deleteBooks(bookId);
+    }
+  }, {
     key: 'render',
     value: function render() {
+
+      var booksList = this.props.books.map(function (booksArr) {
+        return _react2.default.createElement(
+          'option',
+          { key: booksArr._id },
+          booksArr._id
+        );
+      });
+
       return _react2.default.createElement(
         _reactBootstrap.Well,
         null,
@@ -44217,6 +44233,34 @@ var BooksForm = function (_Component) {
             { onClick: this.handleSubmit.bind(this), bsStyle: 'primary' },
             'Save'
           )
+        ),
+        _react2.default.createElement(
+          _reactBootstrap.Panel,
+          { style: { marginTop: '25px' } },
+          _react2.default.createElement(
+            _reactBootstrap.FormGroup,
+            { controlId: '' },
+            _react2.default.createElement(
+              _reactBootstrap.ControlLabel,
+              null,
+              'Select a book id to delete'
+            ),
+            _react2.default.createElement(
+              _reactBootstrap.FormControl,
+              { ref: 'delete', componentClass: 'select', placeholder: 'select' },
+              _react2.default.createElement(
+                'option',
+                { value: 'select' },
+                'select'
+              ),
+              booksList
+            )
+          ),
+          _react2.default.createElement(
+            _reactBootstrap.Button,
+            { onClick: this.onDelete.bind(this), bsStyle: 'danger' },
+            'Delete Book'
+          )
         )
       );
     }
@@ -44225,11 +44269,17 @@ var BooksForm = function (_Component) {
   return BooksForm;
 }(_react.Component);
 
-function mapDispatchToProps(dispatch) {
-  return (0, _redux.bindActionCreators)({ postBooks: _booksActions.postBooks }, dispatch);
+function mapStateToProps(state) {
+  return {
+    books: state.books.books
+  };
 }
 
-exports.default = (0, _reactRedux.connect)(null, mapDispatchToProps)(BooksForm);
+function mapDispatchToProps(dispatch) {
+  return (0, _redux.bindActionCreators)({ postBooks: _booksActions.postBooks, deleteBooks: _booksActions.deleteBooks }, dispatch);
+}
+
+exports.default = (0, _reactRedux.connect)(mapStateToProps, mapDispatchToProps)(BooksForm);
 
 /***/ }),
 /* 490 */
--- src/components/pages/BooksForm.js
+++ src/components/pages/BooksForm.js
@@ -4,7 +4,7 @@ import { Well, Panel, FormControl, FormGroup, ControlLabel, Button } from 'react
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-import { postBooks } from '../../actions/booksActions';
+import { postBooks, deleteBooks } from '../../actions/booksActions';
 
 class BooksForm extends Component {
 
@@ -19,7 +19,20 @@ class BooksForm extends Component {
     this.props.postBooks(book);
   }
 
+  onDelete() {
+    let bookId = findDOMNode(this.refs.delete).value;
+
+    this.props.deleteBooks(bookId);
+  }
+
   render () {
+
+    const booksList = this.props.books.map(booksArr => {
+      return (
+        <option key={booksArr._id}>{booksArr._id}</option>
+      )
+    });
+
     return (
       <Well>
         <Panel>
@@ -49,13 +62,29 @@ class BooksForm extends Component {
           </FormGroup>
           <Button onClick={this.handleSubmit.bind(this)} bsStyle="primary">Save</Button>
         </Panel>
+        <Panel style={{marginTop: '25px'}}>
+          <FormGroup controlId="">
+            <ControlLabel>Select a book id to delete</ControlLabel>
+            <FormControl ref="delete" componentClass="select" placeholder="select">
+              <option value="select">select</option>
+              {booksList}
+            </FormControl>
+          </FormGroup>
+          <Button onClick={this.onDelete.bind(this)} bsStyle="danger">Delete Book</Button>
+        </Panel>
       </Well>
     )
   }
 }
 
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators({postBooks}, dispatch)
+function mapStateToProps(state) {
+  return {
+    books: state.books.books
+  }
 }
 
-export default connect(null, mapDispatchToProps)(BooksForm);
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({postBooks, deleteBooks}, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BooksForm);
