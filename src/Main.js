9db94f3e0982f92c215902c9f74bc617e6a9b0f5
immanoj16 <mmaharana14@gmail.com>
2017-09-21 23:21:51 +0530

    Add: react router

---
+++ src/Main.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Menu from './components/Menu';
+import Footer from './components/Footer';
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <Menu/>
+        {this.props.children}
+        <Footer/>
+      </div>
+    )
+  }
+}
+
+export default Main
--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import { applyMiddleware, createStore } from 'redux';
 import logger from 'redux-logger';
@@ -10,20 +11,27 @@ import logger from 'redux-logger';
 // IMPORT COMBINED REDUCERS
 import reducers from './reducers';
 import BooksList from './components/pages/BooksList';
-import Menu from './components/Menu';
-import Footer from './components/Footer';
+import Cart from './components/pages/Cart';
+import BooksForm from './components/pages/BooksForm';
+import Main from './Main';
 
 // STEP 1 create the store
 const middleware = applyMiddleware(logger);
 const store = createStore(reducers, middleware);
 
-ReactDOM.render(
+const Routes = (
   <Provider store={store}>
-    <div>
-      <Menu />
-      <BooksList />
-      <Footer />
-    </div>
-  </Provider>,
+    <Router history={browserHistory}>
+      <Route path="/" component={Main}>
+        <IndexRoute component={BooksList} />
+        <Route path="/admin" component={BooksForm} />
+        <Route path="/cart" component={Cart} />
+      </Route>
+    </Router>
+  </Provider>
+)
+
+ReactDOM.render(
+  Routes,
   document.getElementById('app')
 );
